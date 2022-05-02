2d3c33a4dc78f83ac7fbfdab1150489da44e5942
Teni17 <103621519+Teni17@users.noreply.github.com>
2022-05-02 14:50:43 -0600

    Add files via upload

--- Track Calories.js
+++ Track Calories.js
@@ -1,28 +1,32 @@
-let user1 = function(foodname, calories){
+document.addEventListener("DOMContentLoaded", function (event) {
+
+ let user1 = function(foodname, calories){
  this.foodname = foodname;
  this.calories = parseInt(calories);
  }
 UserCalorie = [];
-
+});
 function userinfo(){
 let user1= {
  foodname: document.getElementById("foodname").value,
- calories: document.getElementById("Calories").value
-}
+ calories: document.getElementById("Calories").value,
+ }
 document.getElementById("foodname").value = "";
 document.getElementById("Calories").value = "";
 
 UserCalorie.push(user1);
 console.log(UserCalorie);
+
+let num = 0;
+
+for( let i=0; i < UserCalorie.length; i++){
+       num += parseInt(UserCalorie[i].calories ) ;
+    
+}
+document.getElementById("CaloriesTotal").value = num;
 }
 
-function totalcalories(){
-    let calorietotal = 0;
-   // calorietotal = document.getElementById("CaloriesTotal").value;
-   // for(i=0; i < UserCalorie.length; i++){
-        //  calorietotal = UserCalorie[i];
 
-//    }
     
 
-}
+
